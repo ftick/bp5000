a71a71ab865819faea95d704c4c4a2625046a707
Isaiah <isaiah.reimer@mail.utoronto.ca>
2017-03-04 23:12:10 -0500

    add another test

--- tests.py
+++ tests.py
@@ -10,6 +10,12 @@ class test_brackets(TestCase):
         for r in range(1, 16):
             self.assertEqual(m,w[r].llink.wlink.wlink.wlink.wlink.wlink.wlink)
         self.assertEqual(m, w[15].wlink.wlink.wlink.llink.wlink)
-
+    
+    def test2genm(self):
+        w = data.genm(['a','b','c','d'])
+        self.assertEqual(w[0].part1.tag, 'a')
+        self.assertEqual(w[0].part2.tag, 'd')
+        self.assertEqual(w[1].part1.tag, 'b')
+        self.assertEqual(w[1].part2.tag, 'c')
 if __name__ == '__main__':
     unittest.main()
