b66291d2a5f13b54c79ff21944c3dc33de7aea85
Isaiah <isaiah.reimer@mail.utoronto.ca>
2017-03-04 22:54:18 -0500

    improve bracket drawing

--- grf.py
+++ grf.py
@@ -1,10 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def drawmatch(match):
-    str1 = match.part1.tag
-    int1 = match.part1.seed
-    str2 = match.part2.tag
-    int2 = match.part2.seed
+    str1 = match.part1.tag if match.part1 else "TBD"
+    int1 = match.part1.seed if match.part1 else ""
+    str2 = match.part2.tag if match.part2 else "TBD"
+    int2 = match.part2.seed if match.part2 else ""
     img = Image.new('RGBA',(200,80),color=(155,155,255))
     d = ImageDraw.Draw(img)
     font = ImageFont.truetype("/usr/share/fonts/TTF/DejaVuSans.ttf",20)
@@ -15,9 +15,31 @@ def drawmatch(match):
     d.text((60,40), str2)
     return img
 
-import pdb; pdb.set_trace()
+
+def drawbracket(bracket):
+    br = bracket
+    img = Image.new('RGBA',(800, len(br)*120),color=(0,0,0))
+    x = 30
+    ymult = 1
+    while(br[0] is not None):
+        nb = []
+        print(br)
+        y = 30 + (ymult-1)*60
+        for ma in br:
+            im = drawmatch(ma)
+            img.paste(im, (x, y))
+            y += 120*ymult
+            if not (ma.wlink in nb):
+                nb.append(ma.wlink)
+        x += 220
+        ymult = ymult*2
+        br = nb
+    return img
+
+
 if __name__ == '__main__':
     import data
-    b = data.genm(['bob','joe','STEVE','kevin'])
-    img = drawmatch(b[0])
+    b = data.genm(['player1','player2','player3','player4','player5','player6','player7','player8'])
+    img = drawbracket(b)
     img.show()
+
