d9a52d78c9feece2410d5ca24fab2273c0eda72e
Isaiah <isaiahreimer98@gmail.com>
2017-04-28 15:57:23 -0400

    Simple Tournament Management Page

--- gui.py
+++ gui.py
@@ -30,6 +30,13 @@ class MFrame(wx.Frame):
 
         menubar.Append(filem, '&File')
         self.SetMenuBar(menubar)
+        
+        p = wx.Panel(self)
+        self.nb = wx.Notebook(p)
+
+        sz = wx.BoxSizer()
+        sz.Add(self.nb, 1, wx.EXPAND)
+        p.SetSizer(sz)
 
         self.SetSize((950, 650))
         self.SetTitle('BP5000')
@@ -44,7 +51,15 @@ class MFrame(wx.Frame):
         name = wx.TextCtrl(d, pos= (20, 74), value="My Tournament", size=(190,40))
         namel = wx.StaticText(d, pos=(20, 57), label="Tournament Name")
         okbtn = wx.Button(d, label='OK', pos=(30, h-70))
+        def newev(e):
+            d.Close()
+            page = ManagementPage(self.nb, name.GetValue(), sc.GetValue())
+            self.nb.AddPage(page, name.GetValue())
+        d.Bind(wx.EVT_BUTTON, newev, okbtn)
         cancelbtn = wx.Button(d, label='Cancel', pos=(150, h-70))
+        def c(e):
+            d.Close()
+        d.Bind(wx.EVT_BUTTON, c, cancelbtn)
         d.SetSize((250,h))
         d.SetTitle("Create new")
         d.Show(True)
@@ -55,6 +70,14 @@ class MFrame(wx.Frame):
         exit()
 
 
+class ManagementPage(wx.Panel):
+    def __init__(self, parent, name="tournament", elim="2"):
+        wx.Panel.__init__(self, parent)
+        wx.StaticText(self, pos=(20, 30), label="Entrants List (Ordered by seed), 1 per line")
+        elist = wx.TextCtrl(self, pos=(20, 60), size = (500, 500), style = wx.TE_MULTILINE)
+        updatebtn = wx.Button(self, label='Update', pos=(840, 540))
+
 a = wx.App()
 MFrame(None)
-a.MainLoop()    
+a.MainLoop()
+
